bfbb53e2aa3f9beea50daf774c3eeae942f99210
Anubhaw  Sharma <56713895+Anubhaw19@users.noreply.github.com>
2021-05-19 15:17:04 +0530

    GFG refrence

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -66,7 +66,86 @@ void inorder(Node *temp)
 	cout << temp->data << endl;
 	inorder(temp->right);
 }
+/* function to delete the given deepest node
+(delNode) in binary tree */
+void deleteNode(Node *root, Node *delNode)
+{
+	if (root == delNode)
+	{
+		root = NULL;
+		delete (delNode);
+		return;
+	}
+	queue<Node *> q;
+	q.push(root);
+	while (!q.empty())
+	{
+		Node *temp = q.front();
+		q.pop();
+		if (temp == delNode)
+		{
+			temp = NULL;
+			delete (delNode);
+			return;
+		}
+		if (temp->right)
+		{
+			if (temp->right == delNode)
+			{
+				temp->right = NULL;
+				delete (delNode);
+				return;
+			}
+			else
+				q.push(temp->right);
+		}
+		if (temp->left)
+		{
+			if (temp->left == delNode)
+			{
+				temp->left = NULL;
+				delete (delNode);
+				return;
+			}
+			else
+				q.push(temp->left);
+		}
+	}
+}
 
+//function to delete element in a Binary Tree
+Node *deletion(Node *root, int value)
+{
+	if (root == NULL)
+		return NULL;
+
+	if (root->left == NULL && root->right == NULL && root->data == value)
+		return NULL;
+
+	queue<Node *> q;
+	q.push(root);
+	Node *temp;
+	Node *delNode;
+
+	while (!q.empty())
+	{
+		temp = q.front();
+		q.pop();
+		if (temp->data == value)
+			delNode = temp;
+		if (temp->left)
+			q.push(temp->left);
+		if (temp->right)
+			q.push(temp->right);
+	}
+	if (delNode) // i.e, delNode!=NULL
+	{
+		int x = temp->data;
+		delNode->data = x;
+		deleteNode(root, temp);
+	}
+	return root;
+}
 int main()
 {
 	Node *root = createNode(10);
@@ -76,10 +155,11 @@ int main()
 	root->right->left = createNode(15);
 	root->right->right = createNode(8);
 
-	inorder(root);
-	cout << "*******" << endl;
-
 	root = insertNode(root, 102);
 
+	inorder(root);
+
+	root = deletion(root, 102);
+	cout << "*******" << endl;
 	inorder(root);
 }
