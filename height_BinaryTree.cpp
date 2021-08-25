07b74d0422d20d8ebff31136ed8b8c4e5f3fed29
Anubhaw  Sharma <56713895+Anubhaw19@users.noreply.github.com>
2021-08-25 17:27:39 +0530

    Recursive + Iterative Solution

--- height_BinaryTree.cpp
+++ height_BinaryTree.cpp
@@ -1,3 +1,4 @@
+//Recursive Solution
 int maxDepth(Node* root) {
     if(root){
         height = max(maxDepth(root->left), maxDepth(root->right))
@@ -5,3 +6,33 @@ int maxDepth(Node* root) {
     }
     return 0
 }
+//***************************
+
+//Iterative Solution
+
+int maxDepth(TreeNode *root) { 
+    // Base Case 
+    if (root is NULL) 
+        return 0
+    // Create an empty queue for level order tarversal 
+    Queue q
+    q.push(root)
+    int height = 0
+    while (true) {          //instead while(q.size()){ h++; ...}
+        int currLevelNodes = q.size()
+        if (currLevelNodes == 0) 
+            return height
+        height = height + 1
+        while (currLevelNodes > 0) 
+        { 
+            TreeNode node = q.front()
+            q.pop()
+            if (node.left is not NULL) 
+                q.push(node.left)
+            if (node.right is not NULL) 
+                q.push(node.right)
+            currLevelNodes = currLevelNodes - 1
+        }
+    }
+}
+//**********************************
