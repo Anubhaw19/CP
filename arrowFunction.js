f3be8495b54994526e0f0175b4a75d3c6ae39e32
Anubhaw  Sharma <56713895+Anubhaw19@users.noreply.github.com>
2021-10-06 22:00:33 +0530

    Create arrowFunction.js

---
+++ arrowFunction.js
@@ -0,0 +1,17 @@
+<script>
+
+
+    // alert(" hello anubhaw");
+    console.log("DATE:");
+    let da = new Date();
+    console.log(da.getHours());
+    // console.error("hello anubhaw");
+    // console.warn("hello user");
+
+// arrow function 
+    fun = () => {
+        console.log("executing log function");
+        alert("time out");
+    }
+    setTimeout(fun, 3000);
+</script>
