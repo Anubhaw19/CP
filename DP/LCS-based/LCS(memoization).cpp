eaa9bf1e6baeec522d6b8e6a1adb273ca02ed908
anubhaw19 <anubhawapp@gmail.com>
2022-01-02 20:08:59 +0530

    Longest Common Subsequence

--- DP/LCS-based/LCS(memoization).cpp
+++ DP/LCS-based/LCS(memoization).cpp
@@ -1,28 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 //bottom up (memoization)
- // Longest Common Subsequence (LCS)
+// Longest Common Subsequence (LCS)
 int t[1001][1001];
- int LCS(string x, string y, int n,int m)
- {
-if(n==0 || m==0)
-return 0;
-if(t[n][m]!=-1)
-return t[n][m];
-if(x[n-1]==y[m-1])
-return t[n][m]= 1+LCS(x,y,n-1,m-1);
-else
-return t[n][m]=max(LCS(x,y,n-1,m),LCS(x,y,n,m-1));
- }
-
-
+int LCS(string x, string y, int n, int m)
+{
+    if (n == 0 || m == 0)
+        return 0;
+    if (t[n][m] != -1)
+        return t[n][m];
+    if (x[n - 1] == y[m - 1])
+        return t[n][m] = 1 + LCS(x, y, n - 1, m - 1);
+    else
+        return t[n][m] = max(LCS(x, y, n - 1, m), LCS(x, y, n, m - 1));
+}
 
 int main()
-{ memset(t,-1,sizeof(t));
-    string x,y; 
-    cin>>x>>y;
-    int n=x.length();
-    int m=y.length();
-    cout<<LCS(x,y,n,m);
+{
+    memset(t, -1, sizeof(t));
+    string x, y;
+    cin >> x >> y;
+    int n = x.length();
+    int m = y.length();
+    cout << LCS(x, y, n, m);
     return 0;
 }
